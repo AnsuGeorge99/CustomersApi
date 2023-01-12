751725f89a2a97935d25cf1c1349bce0b76fb88c
Ansu George <ansususygeorge@gmail.com>
2023-01-12 17:00:36 +0530

    done

---
+++ CustomersApi/Controllers/CustomersController.cs
@@ -0,0 +1,98 @@
+﻿using CustomersApi.Data.Models;
+using CustomersApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customersService;
+        public CustomersController(ICustomerService customersService)
+        {
+            _customersService = customersService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Customers>> GetCustomers()
+        {
+            try
+            {
+                var customers = await _customersService.GetCustomers();
+                return customers;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        [HttpPost("CustomerPost")]
+        public IActionResult CustomerPost(Customers customers)
+        {
+            try
+            {
+                _customersService.CreateCustomers(customers);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        
+        [HttpPut("CustomerPut")]
+        public IActionResult CustomerPut([FromBody] Customers customers)
+        {
+            try
+            {
+                _customersService.UpdateCustomers(customers);
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("DeleteCustomers/{id}")]
+        public IActionResult DeleteCustomers(int id)
+        {
+            try
+            {
+                _customersService.DeleteCustomers(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost("AdminLogin")]
+        public IActionResult AdminLogin(Login login)
+        {
+            try 
+            { 
+                var result = _customersService.Login(login);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet("GetCustomer/{id}")]
+        public IActionResult GetCustomerById(int id)
+        {
+            return Ok(_customersService.GetCustomersById(id));
+        }
+    }
+}
---
+++ CustomersApi/Services/CustomerService.cs
@@ -0,0 +1,73 @@
+﻿using CustomersApi.Data;
+using CustomersApi.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomersApi.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerDao _customerDao;
+        public CustomerService(ICustomerDao customerDao)
+        {
+            _customerDao = customerDao;
+        }
+        public void CreateCustomers(Customers customers)
+        {
+            Customers values = new Customers();
+            if (customers != null)
+            {
+                values.firstname = customers.firstname;
+                values.lastname = customers.lastname;
+                values.age = customers.age;
+                values.DOB = customers.DOB;
+                values.address = customers.address;
+                _customerDao.Create(customers);
+            }
+        }
+
+        public void DeleteCustomers(int id)
+        {
+            _customerDao.Delete(id);
+        }
+
+        public Task<List<Customers>> GetCustomers()
+        {
+           return _customerDao.GetCustomer();
+        }
+
+        public Customers GetCustomersById(int id)
+        {
+            return _customerDao.GetCustomerById(id).Result;
+        }
+
+        public bool Login(Login login)
+        {
+            return (_customerDao.AdminLogin(login.username, login.password));
+        }
+
+        public void UpdateCustomers(Customers customers)
+        {
+            if (customers != null)
+            {
+                var data = _customerDao.GetCustomer().Result.FirstOrDefault(x => x.CustomerId == customers.CustomerId);
+                Customers result = new Customers();
+                result = data;
+                if (data != null)
+                {
+                    result.firstname = customers.firstname;
+                    result.lastname = customers.lastname;
+                    result.age = customers.age;
+                    result.DOB = customers.DOB;
+                    result.address = customers.address;
+                    _customerDao.Update(result);
+                }
+            }      
+        }
+
+        
+    }
+}
